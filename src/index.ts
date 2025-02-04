2afe54a413111f804ee077e91a4c07617beb54c1
Parker Botsford <parker.botsford.dev@gmail.com>
2025-02-04 15:46:03 -0500

    refactor: use new response object in test html page

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { MPFetch } from '../lib/main';
 
 let app = document.getElementById('app');
 if (app) {
-    const data = await MPFetch.getData({
+    const response = await MPFetch.getData({
         host: 'perimeter',
         storedProc: 'api_custom_My_Giving_History_Widget_HARDCODED',
         requireUser: true,
         debug: true,
     });
 
-    console.log(data);
+    console.debug('response', response);
+    console.debug('response.data', response.data);
+    console.debug('response.error', response.error);
 }
